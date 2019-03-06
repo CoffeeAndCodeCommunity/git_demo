6574d4c33f4dcabaebe19121b5b28e0d4d120c1c
Thomas <thomas@craftacademy.se>
2019-03-06 19:28:14 +0100

    fixes js error by adding async to main event listener

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,8 @@
 const apiUrl = 'https://content.viaplay.se/pc-se/serier/samtliga'
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
-document.addEventListener('DOMContentLoaded', () => {
-  let data = await (await fetch(proxyUrl + apiUrl)).json()
-  console.log(data)
-})
+document.addEventListener('DOMContentLoaded', async () => {
+  let data = await(await fetch(proxyUrl + apiUrl)).json();
+
+  console.log(data);
+});
